ced7caaf2b7e05f4f042c5b9856c9d497f9971b6
Jesús Edel Cereceres Delgado <jesus.edelcereceres@gmail.com>
2016-12-13 23:54:58 +0000

    the first commit is made

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = function (arrayToPermutation) {
+    let auxyliary
+    let length = arrayToPermutation.length
+    for (var i = 0; i < length; i++ ) {
+        
+    }
+}
