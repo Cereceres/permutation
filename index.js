ac70d2846e060321f15dcac565267b702026fd97
Jesús Edel Cereceres Delgado <jesus.edelcereceres@gmail.com>
2016-12-16 17:15:48 +0000

    the repo is init

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 'use strict'
-
+/**
+ * @function
+ * @param {array} - arrayToPermutation 
+ *
+ */
 module.exports = function (arrayToPermutation) {
-    let auxyliary
+    // get the length of array
     let length = arrayToPermutation.length
+    // get the last item
+    let auxyliary = arrayToPermutation[length]
+    // the auxyliary to store the item what change
+    let auxyliary_2 
+    // itereate over all array
     for (var i = 0; i < length; i++ ) {
-        
+        // store the value what changes
+        auxyliary_2 = arrayToPermutation[i]
+        // store the last item stored
+        arrayToPermutation[i] = auxyliary
+        // store in auxyliary value
+        auxyliary = auxyliary_2
     }
 }
